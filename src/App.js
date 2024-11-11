fc8527007f4c2c7c9bb11f7124ce6917af18eddd
devJay123 <morest432@gmail.com>
2024-11-11 15:22:49 +0900

    props를 사용해서 자식 컴포넌트에게 데이터 넘기기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import FruitItem from "./components/FruitItem";
 import { Component } from "./core/core";
 export default class App extends Component {
   constructor() {
@@ -15,12 +16,15 @@ export default class App extends Component {
   render() {
     this.el.innerHTML = /* html */ `
       <h1>Fruits</h1>
-      <ul>
-        ${this.state.fruits
-          .filter((fruit) => fruit.price < 3000)
-          .map((fruit) => `<li>${fruit.name}</li>`)
-          .join("")}
-      </ul>
+      <ul></ul>
     `;
+
+    const ulEl = this.el.querySelector("ul");
+    ulEl.append(
+      ...this.state.fruits.map(
+        (fruit) =>
+          new FruitItem({ props: { name: fruit.name, price: fruit.price } }).el
+      )
+    );
   }
 }
