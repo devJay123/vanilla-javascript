f85124fcf0e5cf2fa768e07632abbfc8bb3c58c9
devJay123 <morest432@gmail.com>
2024-11-11 14:00:03 +0900

    선언적 렌더링, state 사용하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-// export default class App {
-//   constructor() {
-//     this.el = document.createElement("div");
-//     this.el.textContent = "hello world";
-//   }
-// }
-
 import { Component } from "./core/core";
-
 export default class App extends Component {
+  constructor() {
+    super({
+      state: {
+        inputText: "",
+      },
+    });
+  }
+
   render() {
-    this.el.textContent = "hello";
+    this.el.classList.add("search");
+    this.el.innerHTML = /*html*/ ` 
+      <input />
+      <button>Click!</button>
+    `;
+
+    const inputEl = this.el.querySelector("input");
+    inputEl.addEventListener("input", () => {
+      this.state.inputText = inputEl.value;
+    });
+
+    const buttonEl = this.el.querySelector("button");
+    buttonEl.addEventListener("click", () => {
+      console.log(this.state.inputText);
+    });
   }
 }
