aee5cf26286220a40e29029db4f2c8528d98ffa2
devJay123 <morest432@gmail.com>
2024-11-11 13:39:03 +0900

    Component 코어 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-export default class App {
-  constructor() {
-    this.el = document.createElement("div");
-    this.el.textContent = "hello world";
+// export default class App {
+//   constructor() {
+//     this.el = document.createElement("div");
+//     this.el.textContent = "hello world";
+//   }
+// }
+
+import { Component } from "./core/core";
+
+export default class App extends Component {
+  render() {
+    this.el.textContent = "hello";
   }
 }
