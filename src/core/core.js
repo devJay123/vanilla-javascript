8b7ad0473fb61d7c15e78b29d5f64f2834b90198
devJay123 <morest432@gmail.com>
2024-11-11 16:16:17 +0900

    해시로 router 기능 작동하기

--- src/core/core.js
+++ src/core/core.js
@@ -1,3 +1,4 @@
+// Component
 export class Component {
   constructor(payload = {}) {
     const { tagName = "div", state = {}, props = {} } = payload;
@@ -9,4 +10,44 @@ export class Component {
   render() {
     //
   }
+}
+
+// Router
+function routeRender(routes) {
+  if (!location.hash) {
+    history.replaceState(null, "", "/#/");
+  }
+
+  const routerView = document.querySelector("router-view");
+  // http://localhost:1234/#/about?name=kang
+  // #/about?name=kang
+  const [hash, queryString = ""] = location.hash.split("?");
+
+  // a=123&b=456
+  // ['a=123', 'b=456']
+  // 결과 {a: '123', b: '456'}
+  const query = queryString.split("&").reduce((acc, cur) => {
+    const [key, value] = cur.split("=");
+    acc[key] = value;
+    return acc;
+  }, {});
+  history.replaceState(query, "");
+
+  const currentRoute = routes.find((route) => {
+    // /#\/about\/?$/.test('#/about') <- 중간에 변수를 넣을 수 없어서 생성자 이용
+    return new RegExp(`${route.path}/?$`).test(hash);
+  });
+
+  routerView.innerHTML = "";
+  routerView.append(new currentRoute.component().el);
+
+  window.scrollTo(0, 0);
+}
+export function createRouter(routes) {
+  return function () {
+    window.addEventListener("popstate", () => {
+      routeRender(routes);
+    });
+    routeRender(routes); // 최초 호출. popstate는 처음에 동작x
+  };
 }
